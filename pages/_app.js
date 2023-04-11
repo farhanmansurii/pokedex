3131e2a9d6d6d5ca1f8c4e42c7570c7b93a44ea4
Farhan <mansurifarhanfm@gmail.com>
2023-04-12 02:45:50 +0530

    added pokemon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
+import client from '@/apollo/apollo-client'
 import '@/styles/globals.css'
+import { ApolloProvider } from '@apollo/client'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
