37b25ab94d061ae87b9095ce7a1d96a15477fd0c
Farhan <mansurifarhanfm@gmail.com>
2023-04-12 20:47:07 +0530

    fixed

--- pages/[pokemon].js
+++ pages/[pokemon].js
@@ -1,90 +1,100 @@
-
-
-
-
 import client from '@/apollo/apollo-client';
-import Modal from '@/components/Modal';
-import Navbar from '@/components/Navbar';
 import PokemonDetails from '@/components/PokemonDetails';
 import { gql } from '@apollo/client';
-import React, { useState } from 'react'
+import React from 'react';
 
 export default function PokemonDetailPage(props) {
-  const [open, setOpen] = useState()
-  const pokemon = props.data.pokemon
+  const pokemon = props.data.pokemon;
 
   return (
     <div>
-
       <PokemonDetails pokemon={pokemon} />
-
     </div>
-  )
+  );
 }
-
 
 export async function getStaticProps(context) {
-  const { pokemon } = context.params
-  console.log(pokemon)
-  const { data } = await client.query({
-    query: gql`
-      query pokemon($name: String){
-  pokemon(name: $name){
-    id
-    number
-    name
-    weight{
-      minimum
-      maximum
-    }
-    height{
-      minimum
-      maximum
-    }
-    classification
-    types
-    resistant
-    weaknesses
-    fleeRate
-    maxCP
-    maxHP
-    image
+  const { pokemon } = context.params;
+  try
+  {
+    const { data } = await client.query({
+      query: gql`
+        query pokemon($name: String) {
+          pokemon(name: $name) {
+            id
+            number
+            name
+            weight {
+              minimum
+              maximum
+            }
+            height {
+              minimum
+              maximum
+            }
+            classification
+            types
+            resistant
+            weaknesses
+            fleeRate
+            maxCP
+            maxHP
+            image
+          }
+        }
+      `,
+      variables: {
+        name: pokemon,
+      },
+    });
+    return {
+      props: {
+        data,
+      },
+    };
+  } catch (error)
+  {
+    console.error(error);
+    return {
+      props: {
+        data: {},
+      },
+    };
   }
-}`,
-    variables: {
-      name: pokemon,
-    },
-  });
-  return {
-    props: {
-      data
-    },
-  };
 }
-
 
 export async function getStaticPaths() {
   const limit = 20;
 
-  const { data } = await client.query({
-    query: gql`
-      query Pokemons($first: Int!) {
-        pokemons(first: $first) {
-          name
+  try
+  {
+    const { data } = await client.query({
+      query: gql`
+        query Pokemons($first: Int!) {
+          pokemons(first: $first) {
+            name
+          }
         }
-      }
-    `,
-    variables: {
-      first: limit,
-    },
-  });
+      `,
+      variables: {
+        first: limit,
+      },
+    });
 
-  const paths = data.pokemons.map((pokemon) => ({
-    params: { pokemon: pokemon.name },
-  }));
+    const paths = data.pokemons.map((pokemon) => ({
+      params: { pokemon: pokemon.name },
+    }));
 
-  return {
-    paths,
-    fallback: false,
-  };
+    return {
+      paths,
+      fallback: true,
+    };
+  } catch (error)
+  {
+    console.error(error);
+    return {
+      paths: [],
+      fallback: true,
+    };
+  }
 }
